b46bd67bca6d83e5c1749f67048435e24c6e27da
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-01 10:57:54 -0400

    SOlution25 skeleton code

---
+++ exercsie25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,46 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution25 {
+
+    // get password string from user
+    public String getUserString(){
+        /*Scanner input = new Scanner(System.in);
+        System.out.println("Enter password:");
+        return input.next();*/
+    }
+
+    // check if it's at least 8 characters
+    private boolean isMinimumLength(String password){
+        //return (password.length()>=8);
+    }
+    // evaluate the strength of the password based on criteria given
+    public int passwordValidator(String password){
+        // make int passwordStrength
+        //int passwordStrength = 0;
+        //if(this.isMinimumLength(password)){
+        //    hasNumber = false
+        //    hasSpecialCharacter = false
+        //    parse string for number
+        //    if number is found
+        //        passwordStrength = 2
+        //    parse string for special character
+        //    if special character is found
+        //        passwordStrength = 3
+        //else
+        //    try parseInt
+        //       passwordStrength = 0
+        //    catch: passwordStrength = 1
+        }
+
+        //return passwordStrength;
+    }
+
+    public static void main(String[] args){
+        Solution25 sol = new Solution25();
+        String password = sol.getUserString();
+        // switch case (0-3)
+        // print "the password" +password+ "is a" + case + "password"
+    }
+}
