cae2a6e0c405e28c3311aef0fb0479864f4be9ef
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-02 18:44:20 -0400

    29 skeleton

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,24 @@
+package baseline;
+
+public class Solution29 {
+
+    // get user input
+    public String getUserInput(){
+
+    }
+
+    // catch bad inputs in a loop
+    public float checkInput(){
+        // loop a check for 0 and a try catch for parsefloat
+    }
+
+    public short calcTimeToDoubleInvestment(float rate){
+        // year = 72/rate
+    }
+
+    public static void main(String[] args){
+        Solution29 sol = new Solution29();
+        float rate = sol.checkInput();
+        // print calcTimetoDouble(rate)
+    }
+}
