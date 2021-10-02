97c97502dc666ba974b8d02627e602c007731eb2
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-02 18:20:41 -0400

    28 skeleton

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,19 @@
+package baseline;
+
+public class Solution28 {
+
+    // get input from user
+    private int getUserInput(){
+        // prompt for a number to return
+    }
+    // loop the number prompt, add all values to the total
+    public int computeTotal(){
+        int total = 0;
+        // for i while i < 5
+        //      total += getUserInput
+        return total;
+    }
+    public static void main(String[] args){
+        // print computeTotal
+    }
+}
