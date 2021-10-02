b8734a37f02caa478e251af7d9c9eac441ee13d5
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-02 16:15:37 -0400

    exercise27 skeleton

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 David Beers
+ */
+
+package baseline;
+
+import java.util.ArrayList;
+
+public class Solution27 {
+
+    // get an input string from the user
+    public String getUserInput(String prompt{
+
+    }
+
+    private String validateLastName(String name){
+        // return error statement if name is less than 2 characters or blank, otherwise return "no error"
+    }
+
+    private String validateFirstName(String name){
+        // return error statement if name is less than 2 characters or blank, otherwise return "no error"
+    }
+
+    private String validateZip(String zip){
+        // return error statement if zip isn't a 5-digit number, otherwise return "no error"
+    }
+
+    private String validateID(String id){
+        // return error statement if id isn't formatted as AA-1234, otherwise return "no error"
+    }
+
+    public ArrayList<String> validateInput(String fName, String lName, String zip, String id){
+        // ArrayList erros
+        // call validateName for fName, add to errors if it's an error statement
+        // call validateName for lName, add to errors if it's an error statement
+        // call validateZip, add to errors if it's an error statement
+        // call validateID, increment errors if it's an error statement
+        // return errors
+    }
+
+    private void printOutput(ArrayList<String> errors){
+        // use size of errors to determine output format with switch, using elements in errors
+    }
+}
