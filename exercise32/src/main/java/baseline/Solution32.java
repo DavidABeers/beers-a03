7a27fc329179289fcebfbf20f0a62e4926028fe1
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-02 22:28:59 -0400

    32 skeleton

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 David Beers
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution32 {
+    // getUserInput
+    public String getUserInput(String prompt){
+
+    }
+    // check for numeric user input
+    public short checkForNumericInput(String prompt){
+        // loop a try catch
+
+    }
+    //
+    public short getNumberWithDifficulty(short diff){
+        // switch case for diff
+    }
+    // loop for the game
+    private void numberGuess(){
+        short guesses = 0;
+        // the number that has to be guessed
+        short number = getNumberWithDifficulty(checkForNumericInput());
+        short guess = 0;
+        // while guess!=number
+            guess = checkForNumericInput();
+
+
+    }
+}
