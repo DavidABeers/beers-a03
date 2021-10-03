ee6efcebd6d051e53d10dbb601f9090e9fe4f3ef
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-03 16:21:17 -0400

    38 skeleton

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 David Beers
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution38 {
+
+    public String getUserInput(){
+        // return the input
+    }
+
+    public int[] inputToArray(String input){
+        // return the numbers in input as an array
+    }
+    public int[] filterEvenNumbers(int[] numbers){
+        // get even numbers using the modulo operator
+    }
+
+    // prints the array provided
+    public void printArray(int[] array){
+
+    }
+
+    public static void main(String[] args) {
+        Solution38 s = new Solution38();
+        String input = s.getUserInput();
+        int[] original = s.inputToArray(input);
+        int[] evens = s.filterEvenNumbers(original);
+    }
+}
