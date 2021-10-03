eb40b1f7f893243112f1253263699a9e610c6571
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-03 10:11:56 -0400

    finished exerise35

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -6,23 +6,36 @@
 package baseline;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Solution35 {
     // get name from user
     public String getNameFromUser(){
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a name: ");
+        return in.nextLine();
     }
     // build arraylist of names
     private ArrayList<String> makeNameList(){
-        // use a loop
+        ArrayList<String> names = new ArrayList<>();
+        while(true){
+            names.add(this.getNameFromUser());
+            if(names.get(names.size()-1).equals("")){
+                names.remove((names.size()-1));
+                break;
+            }
+        }
+        return names;
     }
     // print out the winner
-    private void selectWInner(ArrayList<String> names){
-        
+    private void selectWinner(ArrayList<String> names){
+        short i = (short)Math.round(Math.random()*names.size());
+        System.out.printf("The winner is... %s!%n", names.get(i));
     }
 
     public static void main(String[] args) {
         Solution35 s = new Solution35();
         ArrayList<String> names = s.makeNameList();
+        s.selectWinner(names);
     }
 }
---
+++ exercise35/src/test/java/baseline/Solution35Test.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 David Beers
+ */
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution35Test {
+    // everything here depends on user input, so a unit test won't work
+    @Test
+    void testMakeNameList() {
+    }
+}
