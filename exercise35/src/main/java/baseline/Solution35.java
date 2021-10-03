e936ba02f2c581f2a4c0e094fd446f6b7b6fa96c
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-03 09:53:05 -0400

    35 skeleton

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 David Beers
+ */
+
+package baseline;
+
+import java.util.ArrayList;
+
+public class Solution35 {
+    // get name from user
+    public String getNameFromUser(){
+
+    }
+    // build arraylist of names
+    private ArrayList<String> makeNameList(){
+        // use a loop
+    }
+    // print out the winner
+    private void selectWInner(ArrayList<String> names){
+        
+    }
+
+    public static void main(String[] args) {
+        Solution35 s = new Solution35();
+        ArrayList<String> names = s.makeNameList();
+    }
+}
