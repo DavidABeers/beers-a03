0c524b3777e797a53c441c95abe8e441df9db411
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-03 09:08:48 -0400

    34 skeleton

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 David Beers
+ */
+
+package baseline;
+
+import java.util.List;
+
+public class Solution34 {
+    // create and fill list
+    private List giveEmployees(){
+
+    }
+
+    // print out the list elements
+    private void printList(List employees){
+
+    }
+
+    // get name to remove
+    private String getName(){
+        
+    }
+
+    // search list for element to remove
+    public List removeEmployee(List employees, String employee){
+
+    }
+
+    public static void main(String[] args) {
+        Solution34 s = new Solution34();
+        List employees = s.giveEmployees();
+        s.printList(employees);
+        String employee = s.getName();
+        s.removeEmployee(employees, employee);
+        s.printList(employees);
+    }
+}
