9fe59bc28d09053013765ce8c73b3178f2f66655
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-03 11:05:05 -0400

    36 skeleton

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,44 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 David Beers
+ */
+
+package baseline;
+
+import java.util.ArrayList;
+
+public class Solution36 {
+    // get a valid input from the user
+    public String getValidInput(){
+
+    }
+
+    // builds data from the inputs
+    private ArrayList<String> buildDataSet(){
+
+    }
+    // gets the mean of the inputs
+    public double average(ArrayList<String> data){
+
+    }
+    // gets the smallest of the input
+    public double min(ArrayList<String> data){
+
+    }
+    // gets the largest of the inputs
+    public double max(ArrayList<String> data){
+
+    }
+    // gets the standard deviation of the inputs
+    public double std(ArrayList<String> data){
+
+    }
+    // prints stats on the list
+    private void printStats(ArrayList<String> data){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
