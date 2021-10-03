457ae0719a21161325c6bd4971d8c270df9a2cae
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-10-03 08:23:58 -0400

    exercise33 skeleton

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 David Beers
+ */
+package baseline;
+
+public class Solution33 {
+    // prompt for get a user's question
+    public String getUserInput(){
+        // print "what's your question?"
+    }
+
+    // print a part of an array depending on a random number generated
+    private void giveRandResponse(){
+        // String array[] responses = {"Yes", "No", "Maybe", "Ask again later."}
+        // print array[(short)Math.round(Math.Random()*4)];
+    }
+    public static void main(String []args){
+        Solution33 s = new Solution33();
+        s.getUserInput();
+        s.giveRandResponse();
+    }
+}
